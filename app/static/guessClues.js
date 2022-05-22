c9ce523591b7a143a9829dbc2e07a4875abb6690
Ben Creighton <23079792@student.uwa.edu.au>
2022-05-23 01:28:45 +0800

    fixing merges

--- app/static/guessClues.js
+++ app/static/guessClues.js
@@ -178,7 +178,4 @@ function submitGuess(checkEnter){
             }
         }
     }
-    guessOutput.innerHTML = checkGuess;
-    trueGuessText.innerHTML = trueGuess;
-    userGuessText.innerHTML = userGuessInput;
 }
